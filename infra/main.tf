db6b60b9d3afd6cd9063e54a86e0b9e7236954ae
Mario Leone <mleone10@gmail.com>
2021-09-03 18:58:39 -0400

    Define lambda function

--- infra/main.tf
+++ infra/main.tf
@@ -1,13 +1,13 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 terraform {
   backend "s3" {
     bucket = "leone-terraform-states"
     key    = "rss-reader.tfstate"
     region = "us-east-1"
   }
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
 
 resource "aws_iam_role" "lambda_role" {
@@ -28,14 +28,21 @@ resource "aws_iam_role" "lambda_role" {
   inline_policy {
     name = "${var.project_name}-policy"
     policy = jsonencode({
-      Version = "2012-10-17"
-      Statement = [
-      ]
+      Version   = "2012-10-17"
+      Statement = []
     })
   }
 }
 
-// TODO: Create bucket for TF state
+resource "aws_lambda_function" "test_lambda" {
+
+  function_name = "${var.project_name}-lambda"
+  filename      = "bin/rssreaderlambda"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "main"
+  runtime       = "go1.x"
+}
+
 // TODO: Test saving state from local to S3
 // TODO: Get remote state working on GitHub
 // TODO: Script artifact upload and lambda deployment
